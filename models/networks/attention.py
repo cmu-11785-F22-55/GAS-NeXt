e62ef95690bedb4ced61d56de9e2ebee7dcdfd9d
xinj10 <xinj10@outlook.com>
2022-11-28 05:31:31 -0500

    refactor: add attention module

---
+++ models/networks/attention.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+
+# Self Attention module from self-attention gan
+class SelfAttention(nn.Module):
+    """Self Attention module from self-attention gan"""
+
+    def __init__(self, in_dim, activation=None):
+        super(SelfAttention, self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+
+        # pointwise convolution
+        self.query_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.key_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.value_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim, kernel_size=1
+        )
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+
+        Args:
+            x (torch.tensor): input feature maps( B X C X W X H)
+
+        Returns:
+            out (torch.tensor) : self attention value + input feature
+            attention (torch.tensor): B X N X N (N is Width*Height)
+        """
+        # print('attention size', x.size())
+        m_batchsize, C, width, height = x.size()
+        # print('query_conv size', self.query_conv(x).size())
+        proj_query = (
+            self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)
+        )  # B X C X (N)
+        proj_key = self.key_conv(x).view(
+            m_batchsize, -1, width * height
+        )  # B X C X (W*H)
+        energy = torch.bmm(proj_query, proj_key)  # transpose check
+        attention = self.softmax(energy)  # B X (N) X (N)
+        proj_value = self.value_conv(x).view(
+            m_batchsize, -1, width * height
+        )  # B X C X N
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma * out + x
+        return out
