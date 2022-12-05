b474a400570866571e38c8745604c7d718b68ff5
xinj10 <xinj10@outlook.com>
2022-12-05 14:45:16 -0500

    feat: add base option

--- options/base_options.py
+++ options/base_options.py
@@ -9,71 +9,30 @@ from util import util
 
 
 class BaseOptions:
+    """This class defines options used during both training and test time.
+    It also implements several helper functions such as parsing, printing, and saving the options.
+    It also gathers additional options defined in <modify_commandline_options> functions in both dataset class and model class.
+    """
+
     def __init__(self):
+        """Reset the class; indicates the class hasn't been initailized"""
         self.initialized = False
 
     def initialize(self, parser):
-        """This class defines options used during both training and test time.
-
-        It also implements several helper functions such as parsing, printing, and saving the options.
-        It also gathers additional options defined in <modify_commandline_options> functions in both dataset class and model class.
-        """
-        parser.add_argument(
-            "--dataroot",
-            required=True,
-            help="path to images (should have subfolders trainA, trainB, valA, valB, etc)",
-        )
-        parser.add_argument(
-            "--batch_size", type=int, default=2, help="input batch size"
-        )
-        parser.add_argument(
-            "--load_size", type=int, default=286, help="scale images to this size"
-        )
-        parser.add_argument(
-            "--crop_size", type=int, default=256, help="then crop to this size"
-        )
-        parser.add_argument(
-            "--input_nc", type=int, default=3, help="# of input image channels"
-        )
-        parser.add_argument(
-            "--output_nc", type=int, default=3, help="# of output image channels"
-        )
-        parser.add_argument("--nz", type=int, default=8, help="#latent vector")
-        parser.add_argument(
-            "--gpu_ids",
-            type=str,
-            default="0",
-            help="gpu ids: e.g. 0  0,1,2, 0,2, -1 for CPU mode",
-        )
+        """Define the common options that are used in both training and test."""
+        # basic parameters
+        parser.add_argument("--dataroot", required=True, help="path to images")
         parser.add_argument(
             "--name",
             type=str,
-            default="",
+            default="experiment_name",
             help="name of the experiment. It decides where to store samples and models",
         )
         parser.add_argument(
-            "--preprocess", type=str, default="resize_and_crop", help="not implemented"
-        )
-        parser.add_argument(
-            "--dataset_mode", type=str, default="aligned", help="aligned,single"
-        )
-        parser.add_argument(
-            "--model",
+            "--gpu_ids",
             type=str,
-            default="bicycle_gan",
-            help="chooses which model to use. bicycle,, ...",
-        )
-        parser.add_argument(
-            "--direction", type=str, default="AtoB", help="AtoB or BtoA"
-        )
-        parser.add_argument(
-            "--epoch",
-            type=str,
-            default="latest",
-            help="which epoch to load? set to latest to use latest cached model",
-        )
-        parser.add_argument(
-            "--num_threads", default=4, type=int, help="# sthreads for loading data"
+            default="0,1",
+            help="gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU",
         )
         parser.add_argument(
             "--checkpoints_dir",
@@ -81,56 +40,12 @@ class BaseOptions:
             default="./checkpoints",
             help="models are saved here",
         )
-        parser.add_argument(
-            "--serial_batches",
-            action="store_true",
-            help="if true, takes images in order to make batches, otherwise takes them randomly",
-        )
-        parser.add_argument(
-            "--use_dropout", action="store_true", help="use dropout for the generator"
-        )
-        parser.add_argument(
-            "--max_dataset_size",
-            type=int,
-            default=float("inf"),
-            help="Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.",
-        )
-        parser.add_argument(
-            "--no_flip",
-            action="store_true",
-            help="if specified, do not flip the images for data argumentation",
-        )
-
         # model parameters
         parser.add_argument(
-            "--num_Ds", type=int, default=2, help="number of Discrminators"
-        )
-        parser.add_argument(
-            "--netD",
+            "--model",
             type=str,
-            default="basic_256_multi",
-            help="selects model to use for netD",
-        )
-        parser.add_argument(
-            "--netD2",
-            type=str,
-            default="basic_256_multi",
-            help="selects model to use for netD2",
-        )
-        parser.add_argument(
-            "--netG", type=str, default="unet_256", help="selects model to use for netG"
-        )
-        parser.add_argument(
-            "--netE",
-            type=str,
-            default="resnet_256",
-            help="selects model to use for netE",
-        )
-        parser.add_argument(
-            "--nef",
-            type=int,
-            default=64,
-            help="# of encoder filters in the first conv layer",
+            default="font_translator_gan",
+            help="chooses which model to use",
         )
         parser.add_argument(
             "--ngf",
@@ -145,35 +60,30 @@ class BaseOptions:
             help="# of discrim filters in the first conv layer",
         )
         parser.add_argument(
+            "--netD",
+            type=str,
+            default="basic",
+            help="specify discriminator architecture",
+        )
+        parser.add_argument(
+            "--netG",
+            type=str,
+            default="resnet_9blocks",
+            help="specify generator architecture",
+        )
+        parser.add_argument(
+            "--n_layers_D", type=int, default=3, help="only used if netD==n_layers"
+        )
+        parser.add_argument(
             "--norm",
             type=str,
             default="instance",
-            help="instance normalization or batch normalization",
-        )
-        parser.add_argument(
-            "--upsample", type=str, default="basic", help="basic | bilinear"
-        )
-        parser.add_argument(
-            "--nl",
-            type=str,
-            default="relu",
-            help="non-linearity activation: relu | lrelu | elu",
-        )
-
-        # extra parameters
-        parser.add_argument(
-            "--where_add",
-            type=str,
-            default="all",
-            help="input|all|middle; where to add z in the network G",
-        )
-        parser.add_argument(
-            "--conditional_D", action="store_true", help="if use conditional GAN for D"
+            help="instance normalization or batch normalization [instance | batch | none]",
         )
         parser.add_argument(
             "--init_type",
             type=str,
-            default="xavier",
+            default="normal",
             help="network initialization [normal | xavier | kaiming | orthogonal]",
         )
         parser.add_argument(
@@ -183,9 +93,55 @@ class BaseOptions:
             help="scaling factor for normal, xavier and orthogonal.",
         )
         parser.add_argument(
-            "--center_crop",
-            action="store_true",
-            help="if apply for center cropping for the test",
+            "--no_dropout", action="store_true", help="no dropout for the generator"
+        )
+        # dataset parameters
+        parser.add_argument(
+            "--dataset_mode",
+            type=str,
+            default="font",
+            help="chooses how datasets are loaded.",
+        )
+        parser.add_argument(
+            "--direction",
+            type=str,
+            default="english2chinese",
+            help="define the style transfer direction, note that evaluate script does not support chinese2english mode",
+        )
+        parser.add_argument(
+            "--num_threads", default=8, type=int, help="# threads for loading data"
+        )
+        parser.add_argument(
+            "--batch_size", type=int, default=1, help="input batch size"
+        )
+        parser.add_argument(
+            "--load_size", type=int, default=286, help="scale images to this size"
+        )
+        parser.add_argument(
+            "--max_dataset_size",
+            type=int,
+            default=float("inf"),
+            help="Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.",
+        )
+
+        # additional parameters
+        parser.add_argument(
+            "--display_winsize",
+            type=int,
+            default=256,
+            help="display window size for both visdom and HTML",
+        )
+        parser.add_argument(
+            "--epoch",
+            type=str,
+            default="latest",
+            help="which epoch to load? set to latest to use latest cached model",
+        )
+        parser.add_argument(
+            "--load_iter",
+            type=int,
+            default="0",
+            help="which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]",
         )
         parser.add_argument(
             "--verbose",
@@ -198,18 +154,13 @@ class BaseOptions:
             type=str,
             help="customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}",
         )
-        parser.add_argument(
-            "--display_winsize", type=int, default=256, help="display window size"
-        )
-
-        # special tasks
         self.initialized = True
         return parser
 
     def gather_options(self):
         """Initialize our parser with basic options(only once).
         Add additional model-specific and dataset-specific options.
-        These options are difined in the <modify_commandline_options> function
+        These options are defined in the <modify_commandline_options> function
         in model and dataset classes.
         """
         if not self.initialized:  # check if it has been initialized
@@ -238,7 +189,6 @@ class BaseOptions:
 
     def print_options(self, opt):
         """Print and save options
-
         It will print both current options and default values(if different).
         It will save options into a text file / [checkpoints_dir] / opt.txt
         """
@@ -256,7 +206,7 @@ class BaseOptions:
         # save to the disk
         expr_dir = os.path.join(opt.checkpoints_dir, opt.name)
         util.mkdirs(expr_dir)
-        file_name = os.path.join(expr_dir, "opt.txt")
+        file_name = os.path.join(expr_dir, "{}_opt.txt".format(opt.phase))
         with open(file_name, "wt") as opt_file:
             opt_file.write(message)
             opt_file.write("\n")
